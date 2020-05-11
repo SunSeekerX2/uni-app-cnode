8c4c549c410660d832f181efae18c3b77db8e196
SunSeekerX <1647800606@qq.com>
2020-05-11 14:55:03 +0800

    update

--- App.vue
+++ App.vue
@@ -1,23 +1,25 @@
 <script>
-	export default {
-		onLaunch: function() {
-			console.log('App Launch')
-		},
-		onShow: function() {
-			console.log('App Show')
-		},
-		onHide: function() {
-			console.log('App Hide')
-		}
+export default {
+	onLaunch: function() {
+		console.log('App Launch')
+	},
+	onShow: function() {
+		console.log('App Show')
+	},
+	onHide: function() {
+		console.log('App Hide')
 	}
+}
 </script>
 
 <style>
-	@import './common/uni.css';
-	@import './static/iconfont/iconfont.css';
-	@import './components/gaoyia-parse/parse.css';
-	page{
-		background-color: #e1e1e1;
-	}
-	image{will-change: transform}
+@import './common/uni.css';
+@import './static/iconfont/iconfont.css';
+@import './components/gaoyia-parse/parse.css';
+page {
+	background-color: #e1e1e1;
+}
+image {
+	will-change: transform;
+}
 </style>
